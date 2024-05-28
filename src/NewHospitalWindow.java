12b27d0241640596ebf52f25b5429823bc157202
nikolayratushnyak <nikolay.ratushnyak03@gmail.com>
2024-05-28 19:04:23 +0700

    fixed

--- src/NewHospitalWindow.java
+++ src/NewHospitalWindow.java
@@ -10,61 +10,69 @@ public class NewHospitalWindow extends JFrame {
 
     public NewHospitalWindow() {
         super("Добавить новую больницу");
+        initComponents();
+    }
+
+    private void initComponents() {
+        setPreferredSize(new Dimension(400, 200));
+        setLocation(100, 100);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.weighty = 1;
+
+        JLabel numberLabel = new JLabel("Номер больницы");
+        numberLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        mainPanel.add(numberLabel, gbc);
+
+        gbc.gridx = 1;
+        hospitalNumberField = new JTextField();
+        mainPanel.add(hospitalNumberField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        JLabel clinicLabel = new JLabel("Привязать клинику");
+        clinicLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        mainPanel.add(clinicLabel, gbc);
+
+        gbc.gridx = 1;
+        clinicComboBox = new JComboBox<>();
         try {
-            // Инициализация компонентов окна
-            setPreferredSize(new Dimension(400, 200));
-            setLocation(100, 100);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-            setLayout(new BorderLayout());
-
-            JPanel mainPanel = new JPanel(new GridBagLayout());
-            GridBagConstraints gbc = new GridBagConstraints();
-            gbc.gridx = 0;
-            gbc.gridy = 0;
-            gbc.gridwidth = 1;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            gbc.weighty = 1;
-
-            // Метка и поле ввода для номера больницы
-            JLabel numberLabel = new JLabel("Номер больницы");
-            numberLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-            mainPanel.add(numberLabel, gbc);
-
-            gbc.gridx = 1;
-            hospitalNumberField = new JTextField();
-            mainPanel.add(hospitalNumberField, gbc);
-
-            // Метка и выпадающий список для выбора клиники
-            gbc.gridx = 0;
-            gbc.gridy = 1;
-            JLabel clinicLabel = new JLabel("Привязать клинику");
-            clinicLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-            mainPanel.add(clinicLabel, gbc);
-
-            gbc.gridx = 1;
-            clinicComboBox = new JComboBox<>();
             populateClinicComboBox();
-            mainPanel.add(clinicComboBox, gbc);
-
-            // Кнопки "ОК" и "Отмена"
-            DialogButtonsPanel dialogButtonsPanel = new DialogButtonsPanel();
-            dialogButtonsPanel.cancelButton.addActionListener(e -> dispose());
-            dialogButtonsPanel.okButton.addActionListener(e -> applyChanges());
-
-            add(mainPanel, BorderLayout.CENTER);
-            add(dialogButtonsPanel, BorderLayout.SOUTH);
-
-            pack();
-            setVisible(true);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(this, "Ошибка при загрузке клиник: " + e.getMessage(), "Ошибка", JOptionPane.ERROR_MESSAGE);
         }
+        mainPanel.add(clinicComboBox, gbc);
+
+        DialogButtonsPanel dialogButtonsPanel = new DialogButtonsPanel();
+        dialogButtonsPanel.cancelButton.addActionListener(e -> dispose());
+        dialogButtonsPanel.okButton.addActionListener(e -> applyChanges());
+
+        add(mainPanel, BorderLayout.CENTER);
+        add(dialogButtonsPanel, BorderLayout.SOUTH);
+
+        pack();
+        setVisible(true);
     }
 
     private void populateClinicComboBox() throws SQLException {
-        clinicMap = GetUtils.getClinics();  // Получение списка клиник из базы данных
-        for (String clinicName : clinicMap.keySet()) {
-            clinicComboBox.addItem(clinicName);
+        clinicMap = new HashMap<>();
+        try (Connection connection = DriverManager.getConnection(ConnectionCnfg.url, ConnectionCnfg.username, ConnectionCnfg.password);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT clinic_id, number FROM \"Clinic\"")) {
+
+            while (resultSet.next()) {
+                int clinicId = resultSet.getInt("clinic_id");
+                String clinicNumber = resultSet.getString("number");
+                clinicMap.put(clinicNumber, clinicId);
+                clinicComboBox.addItem(clinicNumber);
+            }
         }
     }
 
@@ -87,6 +95,19 @@ public class NewHospitalWindow extends JFrame {
             JOptionPane.showMessageDialog(this, "При выполнении запроса произошла ошибка\n" + e.getMessage());
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(this, "Неверный формат номера больницы\n" + e.getMessage());
+        }
+    }
+
+    // Класс для кнопок OK и Cancel
+    class DialogButtonsPanel extends JPanel {
+        JButton okButton;
+        JButton cancelButton;
+
+        DialogButtonsPanel() {
+            okButton = new JButton("OK");
+            cancelButton = new JButton("Cancel");
+            add(okButton);
+            add(cancelButton);
         }
     }
 }
